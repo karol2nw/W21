3dae9123d2ff84784603d50348d1f07061e0bc02
Karol Nowak <karol2nw@anwokaja.pl>
2023-04-21 22:43:36 +0200

    day 13

--- W21/W21/Program.cs
+++ W21/W21/Program.cs
@@ -1,18 +1,48 @@
 ﻿using W21;
 
 Employee employee1 = new Employee ("Andrzej","Kowalski");
-Employee employee2 = new Employee ("Maria","Jaworska");
-Employee employee3 = new Employee("Edward", "Psikuta");
 
-employee1.AddRate('A');
-employee1.AddRate(8);
+Console.WriteLine("Program do oceny pracowników");
+Console.WriteLine("============================");
+Console.WriteLine("aby zakończyć działanie programu wciśnij 'q' ");
 
-var stats = employee1.GetStatistics();
-Console.WriteLine(stats.MaxValue );
+do
+{
+   Console.WriteLine("podaj ocenę pracownika");
+   
+    var input = Console.ReadLine();
+    
+    try 
+    {
+        employee1.AddRate(input);
+    }
+    catch (Exception e)
+    { 
+        Console.WriteLine($"exeption catched : {e.Message}");
+    }
+    
+    if (input == "q")
+    {
+        break;
+    }
+
+}while(true);
+
+var statistic = employee1.GetStatistics();
+
+Console.WriteLine($"ocena pracowanika : {employee1.FirstName} {employee1.LastName}");
+Console.WriteLine($"najwyższa ocena : {statistic.MaxValue} ");
+Console.WriteLine($"najniższa ocena : {statistic.MinValue} ");
+Console.WriteLine($" średnia ocena : {statistic.AverageValue :N2} ");
+Console.WriteLine($"średnia ocena literowa : {statistic.AverageLetter}");
 
 
-List<Employee> employees = new List<Employee>()
-{ 
-    employee1, employee2, employee3 
-};
+
+
+
+
+
+
+
+
 
